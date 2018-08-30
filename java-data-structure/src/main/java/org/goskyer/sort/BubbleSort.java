318ec242a6cc35165c83c6a5cea718290e586024
zhiqin.zhang <zhiqin.zhang@wehotelgloabl.com>
2018-08-30 23:13:03 +0800

    冒泡排序

---
+++ java-data-structure/src/main/java/org/goskyer/sort/BubbleSort.java
@@ -0,0 +1,28 @@
+package org.goskyer.sort;
+
+
+import java.util.stream.Stream;
+
+/**
+ * 冒泡排序-> 比较相邻的2个值的大小 然后进行交换
+ * @description: 冒泡排序实现
+ * @author: monster_x
+ * @create: 2018-08-30 22:42
+ **/
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        Integer[] arr = {5, 6, 0, 88, 99, 10, 5, 3};
+        for (int i = 0; i < arr.length - 1; i++) {//外层循环控制排序趟数
+            for (int j = 0; j < arr.length - 1 - i; j++) {//内层循环控制每一趟排序多少次
+                if (arr[j] > arr[j + 1]) {
+                    //交换值
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        Stream.of(arr).forEach(n -> System.out.print(n + ","));
+    }
+}
---
+++ java-data-structure/src/main/java/org/goskyer/sort/package-info.java
@@ -0,0 +1,6 @@
+package org.goskyer.sort;
+
+
+/**
+ * 排序算法实现
+ */
