8a062eaeb975876b37656c8a2f763a00c1e5330f
zhiqin.zhang <zhiqin.zhang@wehotelgloabl.com>
2019-04-17 23:38:40 +0800

    add: 补充function示例代码

--- java-base/src/main/java/org/goskyer/base/lambda/base/function/Demo.java
+++ java-base/src/main/java/org/goskyer/base/lambda/base/function/Demo.java
@@ -18,13 +18,27 @@ public class Demo {
     List<Integer> collect =
         Stream.of("lambda", "in", "action").map(String::length).collect(Collectors.toList());
     collect.forEach(System.out::println);
+    // 平方
+    int compute = Demo.compute(5, value -> value * value);
+    // 求和
+    int compute1 = Demo.compute(5, value -> value + value);
+    // 求差
+    int compute2 = Demo.compute(5, value -> value - 2);
+    System.out.println(compute);
+    System.out.println(compute1);
+    System.out.println(compute2);
   }
 
   public static <T, R> List<R> map(List<T> list, Function<T, R> f) {
     List<R> result = new ArrayList<>();
     for (T s : list) {
+      // Function作为一个函数式接口，主要方法apply接收一个参数，返回一个值
       result.add(f.apply(s));
     }
     return result;
+  }
+
+  public static int compute(int a, Function<Integer, Integer> function) {
+    return function.apply(a);
   }
 }
