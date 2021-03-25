ff8543bfee7dc13b12f4c861113115c625768e41
zhiqin.zhang <zzqnobody@126.com>
2021-03-25 18:06:59 +0800

    leetcode 1295题

---
+++ java-data-structure/src/main/java/org/goskyer/example/leetcode1295/FindNumbers.java
@@ -0,0 +1,55 @@
+package org.goskyer.example.leetcode1295;
+
+/**
+ * 给你一个整数数组 nums，请你返回其中位数为 偶数 的数字的个数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [12,345,2,6,7896]
+ * 输出：2
+ * 解释：
+ * 12 是 2 位数字（位数为偶数） 
+ * 345 是 3 位数字（位数为奇数）  
+ * 2 是 1 位数字（位数为奇数） 
+ * 6 是 1 位数字 位数为奇数） 
+ * 7896 是 4 位数字（位数为偶数）  
+ * 因此只有 12 和 7896 是位数为偶数的数字
+ * 示例 2：
+ * <p>
+ * 输入：nums = [555,901,482,1771]
+ * 输出：1
+ * 解释：
+ * 只有 1771 是位数为偶数的数字。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 500
+ * 1 <= nums[i] <= 10^5
+ * <p>
+ * <p>
+ */
+public class FindNumbers {
+
+    public static int findNumbers(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int num : nums) {
+            int len = String.valueOf(num).toCharArray().length;
+            if ((len & 1) == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {12, 345, 2, 6, 7896};
+        System.out.println(findNumbers(nums));
+    }
+
+}
