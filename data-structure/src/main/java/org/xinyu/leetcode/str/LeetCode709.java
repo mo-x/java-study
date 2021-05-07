f5261a6a60762dfdeb9c91abbcbf5f1a37bb7264
zhiqin.zhang <zzqnobody@126.com>
2021-05-07 10:12:20 +0800

    LeetCode709 实现字符串转小写

---
+++ data-structure/src/main/java/org/xinyu/leetcode/str/LeetCode709.java
@@ -0,0 +1,24 @@
+package org.xinyu.leetcode.str;
+
+/**
+ * 实现函数 ToLowerCase()，该函数接收一个字符串参数 str，并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
+ */
+public class LeetCode709 {
+
+    public String toLowerCase(String str) {
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] >= 'A' && chars[i] <= 'Z') {
+                chars[i] += 32;
+            }
+        }
+        return new String(chars);
+    }
+
+    public static void main(String[] args) {
+        char a = 'A';
+        char la = 'A' + 32;
+        System.out.println(la);
+
+    }
+}
